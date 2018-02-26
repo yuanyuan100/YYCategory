9953d6e95cb2d9e215ea91fe7916366a6fdfa334
Yvan.Peng <yvan@YvanPengdeMacBook-Pro.local>
2018-02-26 14:17:34 +0800

    [add]UIDevice

---
+++ YYCategory/Classes/UIDevice/UIDevice+YYModel.h
@@ -0,0 +1,12 @@
+//
+//  UIDevice+YYModel.h
+//  YYCategoryKit
+//
+//  Created by Yvan.Peng on 2018/2/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (YYModel)
+
+@end
---
+++ YYCategory/Classes/UIDevice/UIDevice+YYModel.m
@@ -0,0 +1,12 @@
+//
+//  UIDevice+YYModel.m
+//  YYCategoryKit
+//
+//  Created by Yvan.Peng on 2018/2/26.
+//
+
+#import "UIDevice+YYModel.h"
+
+@implementation UIDevice (YYModel)
+
+@end
